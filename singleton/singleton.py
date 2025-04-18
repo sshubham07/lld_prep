f08df974025b5e05ab485ff1d56645465d493444
Shubham Kumar Gupta <shubhamkumargupta@Shubhams-MacBook-Air.local>
2025-04-19 00:31:28 +0530

    Singleton class implemented

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,2 +1,10 @@
 class Singleton:
-    pass
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+s1 = Singleton()
+s2 = Singleton()
+print(s1 is s2)
