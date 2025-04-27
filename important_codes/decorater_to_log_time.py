949d14df533fd0ca90a4eb96058f2872bfdc9c5c
Shubham Kumar Gupta <shubhamkumargupta@Shubhams-MacBook-Air.local>
2025-04-28 03:44:12 +0530

    decorator modified

--- important_codes/decorater_to_log_time.py
+++ important_codes/decorater_to_log_time.py
@@ -3,14 +3,16 @@ import time
 def check_time(func):
     def wrapper(*args, **kwargs):
         t1 = time.time()
-        func(*args, **kwargs)
+        ans = func(*args, **kwargs)
         t2 = time.time()
         print(f"Total time = {t2 - t1:.2f} seconds")
+        return ans
     return wrapper
 
 @check_time
-def calc():
+def calc(a,b):
     time.sleep(10)
     print("Running calc method")
+    return a+b
 
-calc()
+calc(5,10)
