626666a358031f2b83bace26cb0cb97d9a3a2b6a
Shubham Kumar Gupta <shubhamkumargupta@Shubhams-MacBook-Air.local>
2026-09-01 19:20:35 +0530

    Factory class introduced

--- lld_related_examples/basic_principles/Factory_pattern/factory_class.py
+++ lld_related_examples/basic_principles/Factory_pattern/factory_class.py
@@ -1,25 +1,38 @@
 from abc import ABC,abstractmethod
 class State(ABC):
     @abstractmethod
-    def state_name():
+    def capital():
         pass
 
 class Maharashtra(State):
     def __init__(self,name):
-        self.state_name=name
-    def state_name(self):
-        print(f"State Name={self.state_name}")
+        self.state_name=name.lower()
+    def capital(self):
+        print(f"State Capital of {self.state_name} is Mumbai")
 
 class Bihar(State):
     def __init__(self,name):
-        self.state_name=name
-    def state_name(self):
-        print(f"State Name={self.state_name}")
-def make_state_obj(name):
-    if name.lower()=="bihar":
-        return Bihar(name)
-    else:
-        return Maharashtra(name)
+        self.state_name=name.lower()
+    def capital(self):
+        print(f"State Capital of {self.state_name} is Patna")
+class Punjab(State):
+    def __init__(self,name):
+        self.state_name=name.lower()
+    def capital(self):
+        print(f"State Capital of {self.state_name} is Chandigarh")
+
+class Factory:
+    _registry={}
+    @classmethod
+    def register(cls,key,state_cls):
+        cls._registry[key]=state_cls
+    @classmethod
+    def create(cls,name):
+        return cls._registry[name.strip().lower()](name)
+
 name = input("Enter state name ")
-obj = make_state_obj(name)
-print(obj.state_name)
+Factory.register("bihar",Bihar)
+Factory.register("Maharashtra",Maharashtra)
+Factory.register("Punjab",Punjab)
+obj = Factory.create(name)
+obj.capital()
